c3d98f9140f3fc261f968301f2677bc62f10a098
Chris Soueidan (Loogart) <chris@loogart.com>
2025-08-06 12:28:55 -0400

    updated to jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-function loadPartial(id, url) {
-  fetch(url)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById(id).innerHTML = html;
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  loadPartial("navbar-container", "/partials/navbar.html");
-  loadPartial("footer-container", "/partials/footer.html");
+$(function() {
+  $("#navbar-container").load("partials/navbar.html");
+  $("#footer-container").load("partials/footer.html");
 });
 
 /* date updated */
