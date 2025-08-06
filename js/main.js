528a7b9280dd537368ae608dc6f64dc3fe4c6e40
Chris Soueidan (Loogart) <chris@loogart.com>
2025-08-06 12:20:08 -0400

    finalized HTML
    - meta tags and images
    - templatize nav and footer
    - other pages

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-// This file is intentionally left blank.<script>
-  document.getElementById("year").textContent = new Date().getFullYear();
+function loadPartial(id, url) {
+  fetch(url)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadPartial("navbar-container", "/partials/navbar.html");
+  loadPartial("footer-container", "/partials/footer.html");
+});
+
+/* date updated */
+document.getElementById("year").textContent = new Date().getFullYear();
