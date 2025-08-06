c54fbd6191aa6382be69d87f6afae2887f9b7fe4
Chris Soueidan (Loogart) <chris@loogart.com>
2025-08-06 14:04:34 -0400

    FRANCAIS!!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function() {
   $("#navbar-container").load("partials/navbar.html");
   $("#footer-container").load("partials/footer.html");
+    $("#navbar-container-fr").load("../partials/navbar-fr.html");
+  $("#footer-container-fr").load("../partials/footer-fr.html");
 });
 
 /* date updated */
